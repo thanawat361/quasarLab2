d165baf256a093e6e9c5ff038697e92e234d1ae8
thanawat361 <beemrockkung@gmail.com>
2023-12-08 10:04:37 +0700

    q-Input 3 InputIsus

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -14,7 +14,14 @@
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
-
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
       <q-input
         filled
         type="number"
